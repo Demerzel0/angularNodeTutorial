866de78bc729f1fbc7add1263e32138802d7cd79
as_jcotret <julien.cotret@altshift.fr>
2015-01-23 18:08:41 +0100

    ...

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,17 +1,46 @@
 'use strict';
 
-
 // Declare app level module which depends on filters, and services
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.filters',
-  'myApp.services',
-  'myApp.directives',
-  'myApp.controllers',
-  'ngResource'
+var app = angular.module('myApp', [
+    'ngRoute',
+    'ngResource',
+    'myApp.filters',
+    'myApp.services',
+    'myApp.directives',
+    'myApp.controllers'
+
 ]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
-  $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+config(['$routeProvider',
+    function ($routeProvider) {
+        $routeProvider.when('/view1', {
+            templateUrl: 'partials/partial1.html',
+            controller: 'MyCtrl2'
+        });
+        $routeProvider.when('/view2', {
+            templateUrl: 'partials/partial2.html',
+            controller: 'MyCtrl2'
+        });
+        $routeProvider.otherwise({
+            redirectTo: '/view1'
+        });
+    }
+]);
+
+app.factory('Bookmark', function ($resource) {
+    return $resource('http://localhost:1337/bookmark/:id', {
+        id: '@id'
+    }, {
+        update: {
+            method: 'PUT'
+        }
+    }); // Note the full endpoint address
+});
+app.factory('Tag', function ($resource) {
+    return $resource('http://localhost:1337/tag/:id', {
+        id: '@id'
+    }, {
+        update: {
+            method: 'PUT'
+        }
+    }); // Note the full endpoint address
+});
--- assets/js/controllers.js
+++ assets/js/controllers.js
@@ -3,9 +3,118 @@
 /* Controllers */
 
 angular.module('myApp.controllers', []).
-  controller('MyCtrl1', ['$scope',function($scope) {
-      $scope.firstName = "tutu";
-  }])
-  .controller('MyCtrl2', ['$scope',function($scope) {
+controller('MyCtrl1', ['$scope',
+    function ($scope) {
+        $scope.firstName = "tutu";
+    }
+])
+    .controller('MyCtrl2', ['$scope', 'Bookmark', 'Tag',
+        function ($scope, Bookmark, Tag) {
+            // INIT *********************************************
+            /*            var bookmark = Bookmark.get({
+                id: 1
+            }, function () {
+                $scope.test = bookmark.url
+                console.log(bookmark);
+            });*/
+            /*            var entries = Bookmark.query(function () {
+                console.log(entries);
+            });*/
+            /*            
+            console.log(entries);*/
 
-  }]);
+            $scope.updateTags = function () {
+                if ($scope.bookmark) {
+                    console.log($scope.bookmark)
+                    $scope.tags = Tag.query({
+                        ref_bookmark: $scope.bookmark.id
+                    }, function () {
+                        $scope.updateCompleteTagList();
+                    });
+                }
+            };
+            $scope.updateCompleteTagList = function () {
+                $scope.completeTagList = Tag.query(function ($res) {
+                    console.log("coucou", $res)
+                });
+            };
+            $scope.updateCompleteTagList();
+
+            $scope.updateBookmarks = function () {
+                $scope.bookmarks = Bookmark.query();
+            };
+            $scope.updateBookmarks();
+            $scope.filteredBookmarks = $scope.bookmarks;
+            // action *********************************************
+            $scope.$watch('bookmark', function () {
+                $scope.updateTags();
+            });
+            $scope.keypressed = function ($event) {
+                if (event.which === 13) {
+                    $scope.addBookmark();
+                }
+
+            };
+            $scope.filterList = [];
+            $scope.tagFilter = function ($ref_bookmarkId, $tagId) {
+                var i,
+                    tagIndex = $scope.filterList.indexOf($ref_bookmarkId),
+                    tempFilteredBookmark = [];
+// TODO maintenir une structure plus complexe pour les filtre toggle ou pas (sur l'id du tag)
+                // on trouve si c'est un ajout ou une suppression
+                if (tagIndex === -1) {
+                    $scope.filterList.push($ref_bookmarkId);
+                } else {
+                    $scope.filterList.splice($ref_bookmarkId, 1);
+                }
+                // on parcours tous les bookmark et on ajoute dans la nouvelle list tous ceux 
+                for (var key in $scope.bookmarks) {
+                    if ($scope.bookmarks.hasOwnProperty(key)) {
+                        for (i = 0; i < $scope.filterList.length; i++) {
+                            if ($scope.bookmarks[key].id === $scope.filterList[i]) {
+                                tempFilteredBookmark.push($scope.bookmarks[key])
+                                console.log(tempFilteredBookmark)
+                            }
+                        }
+
+                       
+                    }
+                }
+                console.log("filterlist",$scope.filterList)
+                $scope.filteredBookmarks = tempFilteredBookmark;
+
+            };
+            $scope.addBookmark = function () {
+                $scope.bookmark = new Bookmark();
+                $scope.bookmark.url = $scope.bookmarkToAdd;
+                Bookmark.save($scope.bookmark, function ($res) {
+                    console.log("just saved : ", $res)
+                    $scope.bookmark = $res;
+                    $scope.updateBookmarks();
+                });
+            }
+            $scope.addTag = function () {
+                if ($scope.bookmark) {
+                    $scope.tag = new Tag();
+                    $scope.tag.tagName = $scope.tagToAdd;
+                    $scope.tag.ref_bookmark = $scope.bookmark.id;
+                    Tag.save($scope.tag, function ($res) {
+                        console.log("just saved : ", $res)
+                        $scope.updateTags();
+                    });
+                } else {
+                    window.alert("no bookmark created or selected")
+                }
+            }
+            $scope.removeTag = function ($tagId) {
+
+                Tag.delete({
+                    id: $tagId
+                }, function () {
+                    $scope.updateTags();
+                });
+
+            }
+
+        }
+    ]);
